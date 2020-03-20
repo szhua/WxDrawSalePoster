e80060c446bc50e668b554e5be2e7324c1036b38
szhua <389894467@qq.com>
2020-03-20 15:21:54 +0800

    ### 没有提交信息

--- utils/post.js
+++ utils/post.js
@@ -1,55 +1,103 @@
 export default class LastMayday {
   palette() {
 
-    const views =[
+    const views = [
       drawUserName(),
       _headerImg(),
       drawGoodsContainer(),
       drawTitle()
     ]
 
-    const goodsList = []
-    for (let index = 0; index < 8; index++) {
-        goodsList.push({
-            x:index%2,
-            y:Math.floor(index/2)
-        })
+
+    const goods = [{
+        url: 'http://114.215.124.170:9000/leilei/timg.jpeg',
+        name: '汶川县应季琵琶',
+        oldPrice:110,
+        price:89
+      },
+      {
+        url: 'http://114.215.124.170:9000/leilei/timg%20%281%29.jpeg',
+        name: '海南空运山竹',
+        oldPrice:100,
+        price:88
+      },
+      {
+        url: 'http://114.215.124.170:9000/leilei/u=1588644056,926863221&fm=26&gp=0.jpg',
+        name: '绿色天然应季水果大礼包',
+        oldPrice:90,
+        price:89
+      },
+      {
+        url: 'http://114.215.124.170:9000/leilei/0010128947-000000000619938523_3.jpg',
+        name: '大串葡萄，鲜美多汁',
+        oldPrice:80,
+        price:66
+      },
+      {
+        url: 'http://114.215.124.170:9000/leilei/shuichan.jpeg',
+        name: '舟山渔场小黄鱼',
+        oldPrice:24,
+        price:11
+      },
+      {
+        url: 'http://114.215.124.170:9000/leilei/yuyu.jpg',
+        name: '大连黑身鱼新鲜美味!',
+        oldPrice:90,
+        price:11
+      },
+      {
+        url: 'http://114.215.124.170:9000/leilei/libao.jpg',
+        name: '旺旺大礼包,给他们',
+        oldPrice:44,
+        price:28
+      },
+      {
+        url: 'http://114.215.124.170:9000/leilei/shaoji.jpeg',
+        name: '德州扒鸡,买二赠一',
+        oldPrice:78,
+        price:70
+      }
+    ]
+
+
+    for (let index = 0; index < goods.length; index++) {
+      let item = goods[index] 
+      item.x  = index % 2
+      item.y = Math.floor(index / 2)
+      views.push(drawGoods(item)[0])
+      views.push(drawGoods(item)[1])
+      views.push(drawGoods(item)[2])
+      views.push(drawGoods(item)[3])
     }
 
-    goodsList.forEach(item=>{
-       views.push( drawGoods(item)[0])
-       views.push( drawGoods(item)[1])
-       views.push( drawGoods(item)[2])
-       views.push( drawGoods(item)[3])
-    })
-
+ 
     views.push(drawCodeInfo()[0])
     views.push(drawCodeInfo()[1])
     views.push(drawCodeInfo()[2])
 
-    168,255,120
+    168, 255, 120
     const drawR = {
       width: '700rpx',
       height: '1200rpx',
       background: '/images/post-bg.png',
-      views:views
+      views: views
     }
-   
-    return drawR 
+
+    return drawR
 
   }
 }
 
 const startTop = 30;
 const startLeft = 50;
-const rectTop = startTop+140 ;
-const goodsImgStartTop = rectTop +130 ;
-const goodsImgStartLeft = startLeft+30 ;
+const rectTop = startTop + 140;
+const goodsImgStartTop = rectTop + 130;
+const goodsImgStartLeft = startLeft + 30;
 
 
 
-function drawCodeInfo(){
-   const codeImg ={
+function drawCodeInfo() {
+  const codeImg = {
     type: 'image',
     url: '/images/code.jpeg',
     css: {
@@ -61,131 +109,130 @@ function drawCodeInfo(){
       rotate: 0,
       borderRadius: '80rpx',
     }
-   }
-   const title = {
+  }
+  const title = {
     type: 'text',
     text: '云度城购',
     css: {
       top: `${goodsImgStartTop+4*150+56}rpx`,
       left: `${goodsImgStartLeft+200}rpx`,
-      maxLines:2,
+      maxLines: 2,
       fontSize: '30rpx',
-      lineHeight:'50rpx',
+      lineHeight: '50rpx',
       align: 'left',
-      color:'#737069',
+      color: '#737069',
       textAlign: 'left',
     }
-   }
-   const tips = {
+  }
+  const tips = {
     type: 'text',
     text: '长按识别小程序码',
     css: {
       top: `${goodsImgStartTop+4*150+100}rpx`,
       left: `${goodsImgStartLeft+200}rpx`,
-      maxLines:2,
+      maxLines: 2,
       fontSize: '20rpx',
       align: 'left',
-      color:'#737069',
+      color: '#737069',
       textAlign: 'left',
     }
-   }
-   return [codeImg,title,tips]
+  }
+  return [codeImg, title, tips]
 }
 
 
-function drawGoods(goods){
-  const goodsImg =   {
+function drawGoods(goods) {
+  const goodsImg = {
     type: 'image',
-    url: '/images/goods.jpeg',
+    url: goods.url,
     css: {
       top: `${goodsImgStartTop+goods.y*150}rpx`,
       left: `${goodsImgStartLeft+goods.x*290}rpx`,
       width: '108rpx',
-      mode:'aspectFill',
+      mode: 'aspectFill',
       height: '108rpx',
       shadow: '10rpx 10rpx 5rpx #888888',
       rotate: 0,
       borderRadius: '12rpx',
     },
   }
-  const goodsName ={
+  const goodsName = {
     type: 'text',
-    text: '爱的礼物草莓蛋糕',
+    text: goods.name,
     css: {
-      width:'140rpx',
+      width: '140rpx',
       top: `${goodsImgStartTop+goods.y*150}rpx`,
       left: `${goodsImgStartLeft+120+goods.x*290}rpx`,
-      maxLines:2,
+      maxLines: 2,
       fontSize: '20rpx',
-      lineHeight:'25rpx',
+      lineHeight: '25rpx',
       align: 'left',
-      color:'#6a6766',
+      color: '#6a6766',
       textAlign: 'left',
     }
   }
-  const priceOld ={
+  const priceOld = {
     type: 'text',
-    text: '￥156',
+    text: '￥'+goods.oldPrice,
     css: {
-      width:'140rpx',
+      width: '140rpx',
       top: `${goodsImgStartTop+55+goods.y*150}rpx`,
       left: `${goodsImgStartLeft+120+goods.x*290}rpx`,
-      maxLines:1,
+      maxLines: 1,
       fontSize: '18rpx',
-      lineHeight:'20rpx',
+      lineHeight: '20rpx',
       align: 'left',
-      color:'#d8d4c7',
+      color: '#d8d4c7',
       textAlign: 'left',
-      textDecoration:'line-through'
+      textDecoration: 'line-through'
     }
   }
 
-  const price ={
+  const price = {
     type: 'text',
-    text: '￥56',
+    text: '￥'+goods.price,
     css: {
-      width:'140rpx',
+      width: '140rpx',
       top: `${goodsImgStartTop+75+goods.y*150}rpx`,
       left: `${goodsImgStartLeft+120+goods.x*290}rpx`,
-      maxLines:1,
+      maxLines: 1,
       fontSize: '36rpx',
-      lineHeight:'36rpx',
+      lineHeight: '36rpx',
       align: 'left',
-      color:'#fece3a',
+      color: '#fece3a',
       textAlign: 'left',
     }
   }
 
-  return [goodsImg,goodsName,priceOld,price ]
+  return [goodsImg, goodsName, priceOld, price]
 
 }
 
 
-function drawTitle(){
-  const title  ={
+function drawTitle() {
+  const title = {
     type: 'text',
     text: '今日份爆款推荐',
     css: {
       top: `${rectTop+50}rpx`,
       left: `${startLeft+300}rpx`,
-      maxLines:1,
-      fontWeight:'bold',
+      maxLines: 1,
+      fontWeight: 'bold',
       fontSize: '36rpx',
       align: 'center',
-      color:'#30b907',
+      color: '#30b907',
       textAlign: 'center',
     }
   }
-  return title 
+  return title
 }
 
 
 
 
 
-function drawGoodsContainer(){
-  const rect = 
-  {
+function drawGoodsContainer() {
+  const rect = {
     type: 'rect',
     css: {
       width: '600rpx',
@@ -197,45 +244,40 @@ function drawGoodsContainer(){
       color: '#fff'
     }
   }
-  return rect ;
+  return rect;
 }
 
 
-function drawUserName(){
-  
-  const username ={
+function drawUserName() {
+
+  const username = {
     type: 'text',
     text: '云度城购小程序',
     css: {
       top: `${startTop + 46}rpx`,
       left: `${startLeft+100}rpx`,
-      maxLines:1,
-      color:'#fff',
-      fontSize:'30rpx',
+      maxLines: 1,
+      color: '#fff',
+      fontSize: '30rpx',
       textAlign: 'left',
     }
   }
-  return username  
+  return username
 }
 
 
-function _headerImg(){
+function _headerImg() {
   return ({
-      type: 'image',
-      url: '/images/avatar.jpg',
-      css: {
-        top: `${startTop + 20}rpx`,
-        left: `${startLeft}rpx`,
-        width: '90rpx',
-        height: '90rpx',
-        shadow: '10rpx 10rpx 5rpx #888888',
-        rotate: 0,
-        borderRadius: '45rpx',
-      },
+    type: 'image',
+    url: '/images/avatar.jpg',
+    css: {
+      top: `${startTop + 20}rpx`,
+      left: `${startLeft}rpx`,
+      width: '90rpx',
+      height: '90rpx',
+      shadow: '10rpx 10rpx 5rpx #888888',
+      rotate: 0,
+      borderRadius: '45rpx',
+    },
   })
 }
-
-
-
-
-
